46f50bfb8859d0632b584a9d3d4fef40865877c8
Petros Andreou <ru010256@student.reading.ac.uk>
2020-07-20 18:47:56 +0300

    adding files

---
+++ QR Code Scanner/QR Code Scanner/QR Code Scanner.Android/Services/QrScanningService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using QR_Code_Scanner;
+using ZXing.Mobile;
+using Xamarin.Forms;
+using QR_Code_Scanner.Services;
+
+[assembly: Dependency(typeof(QR_Code_Scanner.Droid.Services.QrScanningService))]
+
+namespace QR_Code_Scanner.Droid.Services
+{
+    public class QrScanningService : IQrScanningService
+    {
+        public async Task<string> ScanAsync()
+        {
+            var optionsDefault = new MobileBarcodeScanningOptions();
+            var optionsCustom = new MobileBarcodeScanningOptions();
+
+            var scanner = new MobileBarcodeScanner()
+            {
+                TopText = "Scan the QR Code",
+                BottomText = "Please Wait",
+            };
+
+            var scanResult = await scanner.Scan(optionsCustom);
+            return scanResult.Text;
+        }
+    }
+}
---
+++ QR Code Scanner/QR Code Scanner/QR Code Scanner/Services/IQrScanningService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QR_Code_Scanner.Services
+{
+    public interface IQrScanningService
+    {
+        Task<string> ScanAsync();
+    }
+}
